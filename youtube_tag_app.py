0a5636cbad1e45b46ee9995abe33369940195049
shantanu <shantanu0201singh@gmail.com>
2022-07-06 21:22:55 +0530

    update knn

--- youtube_tag_app.py
+++ youtube_tag_app.py
@@ -22,9 +22,7 @@ def get_form_data():
         return render_template('home.html')
 
 
-@app.route('/predict', methods=['POST'])
-def get_result():
-    return render_template('home.html', check=1, text="bawasir12123123123")
+
 
 
 def get_data(title, similarity=0.7):
@@ -41,8 +39,8 @@ def get_data(title, similarity=0.7):
     else:
         return ",".join(out)
 if __name__ == "__main__":
-    '''tag_rec = TagRecommendation(directory="data")
+    tag_rec = TagRecommendation(directory="data")
     tag_rec.load_data()
     tag_rec.knn_pipe(mode="train", file="knn_tag.pkl", model="", df_or_string="")
-    '''
-    app.run(debug=True)
+
+    #app.run(debug=True)
